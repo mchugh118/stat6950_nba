689d214c4a418ee4d17a9b1f7b31d04d55b255b6
Patrick McHugh <pmchugh002@gmail.com>
2022-03-30 15:38:42 -0400

    added advanced stats

--- Pre-Processing.R
+++ Pre-Processing.R
@@ -5,6 +5,8 @@ playerscv <- read.csv("data/players cv.csv")
 playerssalary <- read.csv("data/players salary.csv")
 playersstat <- read.csv("data/players stat.csv")
 team <- read.csv("data/team.csv")
+advanced_stats <- read.csv("data/NBA_1950to2020_allstats_dataset.csv")
+
 
 # Gather data only from the 2014-15 season
 player_stats <- playersstat[playersstat$Season=="2015-16",]
@@ -21,9 +23,10 @@ players_cv <- playerscv[playerscv$From<= 2015 & playerscv$To>=2016,]
 data <- merge(player_stats, players_cv, by = "Player")
 data <- merge(data, player_salaries, by = "Player")
 data <- data[,c(1,3,4,6:30,34:38,42)]
-write.csv(data,"data/PlayerData.csv", row.names = TRUE)
 
-attach(data)
-summary(lm(BPM ~ OBPM + DBPM))  # R^2 > .999
-summary(lm(WS ~ OWS + DWS))  # R^2 > .999
-summary(lm(TRB. ~ ORB. + DRB.))  # R^2 > .998
+# Make categories based on birthplace: USA/International
+usa <- read.delim("data/usa.txt")[,1]
+data$International = ifelse(data$Place_of_Birth %in% usa, "No", "Yes")
+
+
+write.csv(data,"data/PlayerData.csv", row.names = TRUE)
